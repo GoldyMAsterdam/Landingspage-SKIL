7f38559ac186900b54f670f085ef2be92574a3d8
Tycho Boom <38436@ma-web.nl>
2024-12-13 23:43:36 +0100

    hamburger menu en footer toegevoegd

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const button = document.getElementById('js--menu');
 const navigation = document.getElementById('js--navigation');
 const body = document.getElementById('js--body');
+const closeButton = document.getElementById('js--close');
+const navLinks = document.querySelectorAll('.mainNavListItem > a');
 
+// Open menu
 button.onclick = function() {
     navigation.style.visibility = "visible";
-    navigation.style.opacity = "1"; 
-    body.style.overflow = "hidden";
+    navigation.style.opacity = "1";
+    body.style.overflow = "hidden"; // Prevent scrolling
+    navigation.classList.add('active'); // Add class to show the menu
+    closeButton.classList.add('active'); // Show close button
+    button.classList.add('hidden'); // Hide hamburger menu button
 }
+
+// Close menu
+closeButton.onclick = function() {
+    navigation.style.visibility = "hidden";
+    navigation.style.opacity = "0";
+    body.style.overflow = "auto"; // Enable scrolling
+    navigation.classList.remove('active'); // Hide the menu
+    closeButton.classList.remove('active'); // Hide close button
+    button.classList.remove('hidden'); // Show hamburger menu button
+}
+
+// Close menu and enable scrolling when a link is clicked
+navLinks.forEach(link => {
+    link.onclick = function() {
+        navigation.style.visibility = "hidden";
+        navigation.style.opacity = "0";
+        body.style.overflow = "auto"; // Enable scrolling
+        navigation.classList.remove('active'); // Hide the menu
+        closeButton.classList.remove('active'); // Hide close button
+        button.classList.remove('hidden'); // Show hamburger menu button
+    }
+});
